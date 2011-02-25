474dd56fa9d8791eaaa4e41587476f350dc5c932
ram <ramusus@gmail.com>
2011-02-26 01:29:10 +0300

    fixed error in urls

--- messages_ajax/urls.py
+++ messages_ajax/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
 
-urlpatterns = patterns('views',
+urlpatterns = patterns('messages_ajax.views',
     url(r'^mark_read/(?P<message_id>\d+)/$', 'message_mark_read', name='message_mark_read'),
 )
